fd684c5be506d7fb99bf3a863a887a49908f60d5
Qoryogdiyev <asadbekqoryogdiyev7@gmail.com>
2023-02-07 17:46:34 +0300

    add text

---
+++ js/index.js
@@ -0,0 +1,47 @@
+var elButton1 = document.querySelector(".faqs__btn--one");
+var elfaqs__text1 = document.querySelector(".faqs__text1");
+
+elButton1.addEventListener("click", function (evt) {
+  elfaqs__text1.classList.toggle("faqs__text");
+  elButton1.classList.toggle("faqs__open__btn");
+});
+
+var elButton2 = document.querySelector(".faqs__btn--two");
+var elfaqs__text2 = document.querySelector(".faqs__text2");
+
+elButton2.addEventListener("click", function (evt) {
+  elfaqs__text2.classList.toggle("faqs__text");
+  elButton2.classList.toggle("faqs__open__btn");
+});
+
+var elButton3 = document.querySelector(".faqs__btn--three");
+var elfaqs__text3 = document.querySelector(".faqs__text3");
+
+elButton3.addEventListener("click", function (evt) {
+  elfaqs__text3.classList.toggle("faqs__text");
+  elButton3.classList.toggle("faqs__open__btn");
+});
+
+var elButton4 = document.querySelector(".faqs__btn--four");
+var elfaqs__text4 = document.querySelector(".faqs__text4");
+
+elButton4.addEventListener("click", function (evt) {
+  elfaqs__text4.classList.toggle("faqs__text");
+  elButton4.classList.toggle("faqs__open__btn");
+});
+
+var elButton5 = document.querySelector(".faqs__btn--fiwe");
+var elfaqs__text5 = document.querySelector(".faqs__text5");
+
+elButton5.addEventListener("click", function (evt) {
+  elfaqs__text5.classList.toggle("faqs__text");
+  elButton5.classList.toggle("faqs__open__btn");
+});
+
+var elButton6 = document.querySelector(".faqs__btn--six");
+var elfaqs__text6 = document.querySelector(".faqs__text6");
+
+elButton6.addEventListener("click", function (evt) {
+  elfaqs__text6.classList.toggle("faqs__text");
+  elButton6.classList.toggle("faqs__open__btn");
+});
